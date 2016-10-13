db29c2f5f0cf6abfbe1a84996fd237b1c1e6bcda
Jack Baron <yodude107@gmail.com>
2016-10-13 22:40:52 +0100

    Testing FS

--- app/js/app.js
+++ app/js/app.js
@@ -7,59 +7,11 @@
 // Define imports
 const {ipcRenderer, remote} = require('electron')
 const {Menu} = remote
+const fs = remote.require('fs')
 
-const template = [
-  {
-    label: 'Edit',
-    submenu: [
-      
-      {
-        role: 'cut'
-      },
-      {
-        role: 'copy'
-      },
-      {
-        role: 'paste'
-      },
-      {
-        role: 'delete'
-      },
-      {
-        role: 'selectall'
-      }
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {
-        label: 'Reload',
-        accelerator: 'CmdOrCtrl+R',
-        click (item, focusedWindow) {
-          if (focusedWindow) focusedWindow.reload()
-        }
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'togglefullscreen'
-      }
-    ]
-  },
-  {
-    role: 'window',
-    submenu: [
-      {
-        role: 'minimize'
-      },
-      {
-        role: 'close'
-      }
-    ]
-  }
-]
+const template = [{label:'Edit',submenu:[{role:'cut'},{role:'copy'},{role:'paste'},{role:'delete'},{role:'selectall'}]},{label:'View',submenu:[{label:'Reload',accelerator:'CmdOrCtrl+R',click(item,focusedWindow){if(focusedWindow)focusedWindow.reload()}},{type:'separator'},{role:'togglefullscreen'}]},{role:'window',submenu:[{role:'minimize'},{role:'close'}]}]
 
 const menu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(menu)
+
+console.log(fs.readdirSync(__dirname))
