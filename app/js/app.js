3770465ff8e56a3108777aef5bb2f027b4f6f5c0
Jack Baron <yodude107@gmail.com>
2016-10-16 22:29:21 +0100

    Frontend work

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ const {Menu, dialog} = remote
 const fs = remote.require('fs')
 
 // App Menu
-const template = [{label:'Edit',submenu:[{role:'cut'},{role:'copy'},{role:'paste'},{role:'delete'},{role:'selectall'}]},{label:'View',submenu:[{label:'Reload',accelerator:'CmdOrCtrl+R',click(item,focusedWindow){if(focusedWindow)focusedWindow.reload()}},{type:'separator'},{role:'togglefullscreen'}]},{role:'window',submenu:[{role:'minimize'},{role:'close'}]}]
+const template = [{label:'Edit',submenu:[{role:'cut'},{role:'copy'},{role:'paste'},{role:'delete'},{role:'selectall'}]},{label:'View',submenu:[{label:'Reload',accelerator:'CmdOrCtrl+R',click(item,focusedWindow){if(focusedWindow)focusedWindow.reload()}}]},{role:'window',submenu:[{role:'minimize'},{role:'close'}]}]
 const menu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(menu)
 
